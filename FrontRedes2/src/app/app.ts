df7fe29798beda57c5b9496d5b56c01a1cf56990
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2025-07-01 23:10:45 -0300

    a

--- FrontRedes2/src/app/app.ts
+++ FrontRedes2/src/app/app.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: false,
   styleUrl: './app.scss'
 })
 export class App {
   protected title = 'FrontRedes2';
+  apiUrl: string = '';
+  querySql: string = '';
+  resposta: any;
+
+  constructor(private apiService: ApiService) { }
+
+  enviarQuery() {
+    if (!this.apiUrl || !this.querySql) {
+      alert('Preencha a URL da API e a query.');
+      return;
+    }
+
+    this.resposta = "";
+
+    this.apiService.executarQuery(this.apiUrl, this.querySql).subscribe({
+      next: (data: any) => {
+        this.resposta = data;
+      },
+      error: (error: { error: any; }) => {
+        console.error('Erro ao chamar API:', error);
+        this.resposta = JSON.stringify(error.error);
+      }
+    });
+  }
 }
