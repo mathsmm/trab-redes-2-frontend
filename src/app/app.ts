358a9f1b535d9d49e06ff803b98f7c1e5474ed05
Matheus Marchi Moro <71672437+mathsmm@users.noreply.github.com>
2025-07-06 13:57:46 -0300

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,7 +9,7 @@ import { ApiService } from './api-service';
 })
 export class App {
   protected title = 'FrontRedes2';
-  apiUrl: string = '';
+  apiUrl: string = 'https://trab-redes-2-api.zeabur.app';
   querySql: string = '';
   resposta: any;
 
